44bc36235f1c2db129c8408b7f8720107582443d
jpinyot <jpinyot@student.42.fr>
2020-10-01 18:42:17 +0200

    input parsing working, need more testing and setting correct errors

--- includes/inputParser.h
+++ includes/inputParser.h
@@ -6,7 +6,7 @@
 /*   By: jpinyot <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/01 11:46:13 by jpinyot           #+#    #+#             */
-/*   Updated: 2020/10/01 16:08:48 by jpinyot          ###   ########.fr       */
+/*   Updated: 2020/10/01 18:32:25 by jpinyot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include <vector>
 
 const char k_commentChar = '#';
-const int k_invalidSize = -1;
+const int k_invalidSize = 0;
 const int k_minSize = 3;
 const int k_maxSize = 12;
 const int k_minValue = 0;
@@ -35,11 +35,20 @@ class InputParser
 		vector<unsigned char>	puzzle_;
 		/* vector<vector<unsigned char> >	vect; */
 		InputErros		errors_;
-		char			size_;
+		unsigned char		size_;
+		unsigned char		maxValidTile_;
+		vector<bool>		tileChecker_;
+
+		void		setTileChecker();
+		/* const bool	tileExist(const &unsigned char pos) const {return (tileChecker_[pos]);} */
+		const bool	tileIsValid(const unsigned char& tile) const
+		{return (tile < maxValidTile_ && !tileChecker_[tile]);}
+		void		setTileAsChecket(const unsigned char& pos) {tileChecker_.at(pos) = true;}
+
 
 		/* const bool fileIsValid( */
 		void		puzzleFromFile(const char* string);
-		void		parseLine(const string& line);
+		InputErros	parseLine(const string& line);
 		InputErros	parseSize(const string& line);
 		bool		errorsInFormat(const string& line, int pos);
 		bool		sizeIsValid() {return (size_ >= k_minSize && size_ <= k_maxSize);};
@@ -50,7 +59,7 @@ class InputParser
 		/* }; */
 		InputParser(const char* string) : /* File descriptor */
 		/* InputParser(const char* string, const int& size) : /1* File descriptor *1/ */
-			puzzle_(0), errors_(noFile), size_(k_invalidSize)
+			puzzle_(0), errors_(noFile), size_(k_invalidSize), maxValidTile_(k_invalidSize), tileChecker_(0)
 		{
 			puzzleFromFile(string);
 		};
--- srcs/inputParser.cpp
+++ srcs/inputParser.cpp
@@ -6,7 +6,7 @@
 /*   By: jpinyot <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/01 11:55:44 by jpinyot           #+#    #+#             */
-/*   Updated: 2020/10/01 16:24:11 by jpinyot          ###   ########.fr       */
+/*   Updated: 2020/10/01 18:31:30 by jpinyot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,32 +18,42 @@
 
 const string k_validChar = "0123456789";
 
-void		InputParser::parseLine(const string& line)
+static int	findLastDigit(const string& line, int i)
 {
+	while (i < line.size() && k_validChar.find(line[i]) != string::npos) {
+		i++;
+	}
+	return (i);
+}
+
+void		InputParser::setTileChecker()
+{
+	maxValidTile_ = size_ * size_;
+	tileChecker_ = vector<bool>(maxValidTile_, false);
+}
+
+InputErros	InputParser::parseLine(const string& line)
+{
+	int count = 0;
 	for (int i = 0; i < line.size(); i++) {
 		if (line.at(i) == k_commentChar) {
 			break ;
 		}
-		/* if (size_ == -1) { */
-		/* 	size_ = stoi(line); */
-		/* 	/1* sizeLine = true; *1/ */
-		/* } */
-	}
-}
-
-bool		InputParser::errorsInFormat(const string& line, int pos)
-{
-	if (pos != string::npos) {
-		while (++pos < line.size()) {
-			if (line.at(pos) == k_commentChar) {
-				return (noError);
-			}
-			else if (line.at(pos) != ' ' && line.at(pos) != '\t') {
+		else if (line.at(i) != ' ' && line.at(i) != '\t') {
+			int tile = stoi(line.substr(i));
+			if (!tileIsValid(tile)) {
 				return (badFormat);
 			}
+			setTileAsChecket(tile);
+			puzzle_.emplace_back(tile);
+			i = findLastDigit(line, i);
+			count += 1;
 		}
 	}
-	return (noError);
+	if (count == size_ || count == 0) {
+		return (noError);
+	}
+	return (badFormat);
 }
 
 InputErros	InputParser::parseSize(const string& line)
@@ -57,12 +67,28 @@ InputErros	InputParser::parseSize(const string& line)
 			if (!sizeIsValid()) {
 				return (invalidSize);
 			}
-			else if (errorsInFormat(line, line.substr(i)
-						.find_last_not_of(k_validChar) + i)) {
+			else if (errorsInFormat(line, findLastDigit(line, i))) {
 				return (badFormat);
 			}
 			else {
+				setTileChecker();
 				return (noError);
+			}
+		}
+	}
+	return (noError);
+}
+
+bool		InputParser::errorsInFormat(const string& line, int pos)
+{
+	if (pos != string::npos) {
+		while (++pos < line.size()) {
+			if (line.at(pos) == k_commentChar) {
+				return (noError);
+			}
+			else if (line.at(pos) != ' ' && line.at(pos) != '\t') {
+				/* cout << "<<" << line << ">>"; */
+				return (badFormat);
 			}
 		}
 	}
@@ -77,12 +103,19 @@ void	InputParser::puzzleFromFile(const char* fileName)
 	/* if (fileName. */
 	while (getline(infile, line)) {
 		if (size_ == k_invalidSize) {
-			
 			if ((errors_ = parseSize(line)) != noError) {
+				cout << "ERROR PARSING SIZE " << errors_;
 				break ;
-				cout << "ERROR PARSING SIZE";
 			}
 		}
-		parseLine(line);
+		else if ((errors_ = parseLine(line)) != noError) {
+		/* cout << line; */
+			cout << "<<ERROR PARSING LINE>>";
+			break ;
+		}
 	}
+	for (auto &tile : puzzle_) {
+		cout << static_cast<int>(tile) << "-";
+	}
+	cout << '\n';
 }
