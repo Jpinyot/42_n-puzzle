5df60fa8d113981f8d0fe1990e001e637d268847
mfigueras11 <mfigueras11@gmail.com>
2020-10-05 11:21:42 +0200

    implemeted destructors for open and closed stacks

--- includes/closedStack.h
+++ includes/closedStack.h
@@ -6,7 +6,7 @@
 /*   By: mfiguera <mfiguera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/28 18:19:24 by jpinyot           #+#    #+#             */
-/*   Updated: 2020/10/02 11:13:27 by mfiguera         ###   ########.fr       */
+/*   Updated: 2020/10/05 11:18:49 by mfiguera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,19 @@ class ClosedStack
 		ClosedStack():
 			stack_(k_maxScore, NULL)
 		{;}
+
+		~ClosedStack()
+		{
+			for(std::vector<State *>::iterator it = stack_.begin(); it != stack_.end(); ++it) {
+				State *currState = *it;
+				State *nextState;
+				while(currState) {
+					nextState = currState->getNext();
+					delete currState;
+					currState = nextState;
+				}
+			}
+		};
 
 		const bool	stateIsClosed(State* state) const;
 		void		addState(State* state);
--- includes/openStack.h
+++ includes/openStack.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   openStack.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mfiguera <mfiguera@student.42.us.org>      +#+  +:+       +#+        */
+/*   By: mfiguera <mfiguera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/30 11:45:15 by mfiguera          #+#    #+#             */
-/*   Updated: 2020/10/01 10:28:36 by jpinyot          ###   ########.fr       */
+/*   Updated: 2020/10/05 11:20:39 by mfiguera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,18 @@ class OpenStack
         OpenStack()
         {
             top_ = NULL;
+        }
+
+        ~OpenStack()
+        {
+            State *currState = top_;
+            State *nextState;
+
+            while (currState) {
+                nextState = currState->getNext();
+                delete currState;
+                currState = nextState;
+            }
         }
 
         void    addState(State *state);
